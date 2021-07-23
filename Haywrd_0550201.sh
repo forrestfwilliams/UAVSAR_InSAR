4e37d5e66c9c69d16c2ec78335803ef379632e6e
Forrest Williams <forrestfwilliams@icloud.com>
2021-07-23 16:21:54 +1200

    download UAVSAR SLC attempt 2

--- Haywrd_0550201.sh
+++ Haywrd_0550201.sh
@@ -1,43 +1,41 @@
-#!/bin/bash
-
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_01_BC.dop -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_01_BC_s1_2x8.llh -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_01_BC_s1_2x8.lkv -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09007_002_090220_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09007_002_090220_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09092_006_091119_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09092_006_091119_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10008_001_100115_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10008_001_100115_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10024_005_100301_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10024_005_100301_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10031_004_100423_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10031_004_100423_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10080_003_101110_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10080_003_101110_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11048_010_110712_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11048_010_110712_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11071_004_111103_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11071_004_111103_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12015_002_120413_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12015_002_120413_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12126_009_121102_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12126_009_121102_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14065_007_140528_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14065_007_140528_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14165_005_141112_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14165_005_141112_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_15150_004_151008_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_15150_004_151008_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_16032_006_160324_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_16032_006_160324_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17044_010_170403_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17044_010_170403_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17114_008_171020_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17114_008_171020_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_18039_006_180801_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_18039_006_180801_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20024_029_200910_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20024_029_200910_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20028_005_201007_L090HH_01_BC_s1_1x1.slc -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
-wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20028_005_201007_L090HH_01_BC.ann -P /u/sar-r2/fwilliam/Calif/UAVSAR/Stacks/Haywrd_05502_01-S1
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_01_BC.dop
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_01_BC_s1_2x8.llh
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_01_BC_s1_2x8.lkv
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09007_002_090220_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09007_002_090220_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09092_006_091119_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_09092_006_091119_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10008_001_100115_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10008_001_100115_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10024_005_100301_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10024_005_100301_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10031_004_100423_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10031_004_100423_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10080_003_101110_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_10080_003_101110_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11048_010_110712_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11048_010_110712_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11071_004_111103_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_11071_004_111103_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12015_002_120413_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12015_002_120413_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12126_009_121102_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_12126_009_121102_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14065_007_140528_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14065_007_140528_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14165_005_141112_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_14165_005_141112_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_15150_004_151008_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_15150_004_151008_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_16032_006_160324_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_16032_006_160324_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17044_010_170403_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17044_010_170403_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17114_008_171020_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_17114_008_171020_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_18039_006_180801_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_18039_006_180801_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20024_029_200910_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20024_029_200910_L090HH_01_BC.ann
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20028_005_201007_L090HH_01_BC_s1_1x1.slc
+wget https://downloaduav2.jpl.nasa.gov/Release25/Haywar_05502_01/Haywar_05502_20028_005_201007_L090HH_01_BC.ann
